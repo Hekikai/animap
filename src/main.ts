4a61080e98e882422073e0353faf23a47478772c
Katana <katanaosu@vk.com>
2022-06-06 18:08:13 +0300

    Fixed setting variables to change global properties (in vite.config.ts) with help by modifyVars

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import axios from "axios";
 import VueAxios from "vue-axios";
-import {Carousel} from "ant-design-vue";
+import {Carousel, Button} from "ant-design-vue";
 import './style/reset.css';
 import './style/global.scss';
+import 'ant-design-vue/dist/antd.less'
 
 import App from './App.vue';
 import router from './router';
@@ -14,6 +15,7 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(Carousel);
+app.use(Button);
 app.use(VueAxios, axios);
 app.provide('axios', app.config.globalProperties.axios);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,32 @@
-import { fileURLToPath, URL } from 'url';
+import {fileURLToPath, URL} from 'url';
 
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-    vueJsx(),
-      Components({
-        resolvers: [AntDesignVueResolver()],
-      })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        vueJsx(),
+
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            less: {
+                modifyVars: {
+                    'primary-color': '#1DA57A',
+                    'link-color': '#1DA57A',
+                    'border-radius-base': '2px',
+                    'font-size-base': '20px'
+                },
+                javascriptEnabled: true
+            }
+        }
     }
-  }
 })
