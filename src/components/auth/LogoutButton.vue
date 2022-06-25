9029a4266ffb619fcd88428753f1a224bb2e17bd
Katana <katanaosu@vk.com>
2022-06-25 18:07:32 +0300

    Added some styles to EntryPage.vue, extract logout button

---
+++ src/components/auth/LogoutButton.vue
@@ -0,0 +1,34 @@
+<template>
+	<a-button @click="handleLogout" type="warning">
+		Logout
+	</a-button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import AuthService from "@/services/auth.service";
+import router from "@/router";
+
+export default defineComponent({
+	name: 'LogoutButton',
+	setup() {
+
+		const handleLogout = () => AuthService.logout().then(
+				() => {
+					router.push({path: '/login'});
+				},
+				(error) => {
+					// TODO: handle error on logout
+				}
+		);
+
+		return {
+			handleLogout
+		}
+	}
+})
+</script>
+
+<style scoped>
+
+</style>
